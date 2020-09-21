c645320bb5f2617de4b31c54bfda524e17df2c2a
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2020-09-21 20:47:32 +0900

    회원 정보 C
    Former-commit-id: 28b498ab2a47bb98cb16297ad4c4d6929c6cf938

--- Back-end/src/main/java/com/ai/brain/controller/UserinfoController.java
+++ Back-end/src/main/java/com/ai/brain/controller/UserinfoController.java
@@ -1,17 +1,15 @@
 package com.ai.brain.controller;
 
-
 import com.ai.brain.service.UserinfoService;
 import com.ai.brain.vo.UserIdPw;
 import com.ai.brain.vo.Userinfo;
-import org.apache.catalina.User;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("Userinfo")
@@ -20,34 +18,42 @@ public class UserinfoController {
     @Autowired
     private UserinfoService userinfoService;
 
-    @GetMapping(value = "/{upk}")
-    public ResponseEntity<Userinfo> getUserinfo(@PathVariable("upk") int upk) {
-//        List<Userinfo> userinfo = userinfoService.findAllByUpk(1);
-        System.out.println("ss");
-
-        Userinfo userinfo = userinfoService.findByUpk(upk);
-        System.out.println(userinfo);
-        System.out.println(userinfo);
-        System.out.println("ss");
-        return new ResponseEntity<>(null, HttpStatus.OK);
-    }
-
-    // 전체 회원 정보 출력
-    @GetMapping("all")
-    public ResponseEntity<List<Userinfo>> selectAll() {
-        List<Userinfo> list = userinfoService.findAll();
-
-        return new ResponseEntity<List<Userinfo>>(list, HttpStatus.OK);
-    }
-
     // 회원 가입
-    @GetMapping("/join")
-    public ResponseEntity<Userinfo> join(@RequestBody UserIdPw userIdPw) {
+    @PostMapping("/join")
+    @ApiOperation(value = "회원 가입")
+    public ResponseEntity<HashMap<String, Object>> join(@RequestBody UserIdPw userIdPw) {
         System.out.println("join Controller");
-        userinfoService.join(userIdPw);
-
-        return new ResponseEntity<>(null, HttpStatus.OK);
+        try {
+            HashMap<String, Object> map = new HashMap<>();
+            Userinfo userinfo = userinfoService.join(userIdPw);
+            map.put("Userinfo", userinfo.toString());
+            return new ResponseEntity<HashMap<String, Object>>(map, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
+
+//
+//    @GetMapping(value = "/{upk}")
+//    public ResponseEntity<Userinfo> getUserinfo(@PathVariable("upk") int upk) {
+////        List<Userinfo> userinfo = userinfoService.findAllByUpk(1);
+//        System.out.println("ss");
+//
+//        Userinfo userinfo = userinfoService.findByUpk(upk);
+//        System.out.println(userinfo);
+//        System.out.println(userinfo);
+//        System.out.println("ss");
+//        return new ResponseEntity<>(null, HttpStatus.OK);
+//    }
+//
+//    // 전체 회원 정보 출력
+//    @GetMapping("all")
+//    public ResponseEntity<List<Userinfo>> selectAll() {
+//        List<Userinfo> list = userinfoService.findAll();
+//
+//        return new ResponseEntity<List<Userinfo>>(list, HttpStatus.OK);
+//    }
+
 
     // PK로 회원 불러오기
 //    @GetMapping("/{upk}")
