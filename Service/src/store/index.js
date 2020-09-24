af6530a24627933a9d82b8c440e4e77e7caa7de4
김승환 <bwin9509@naver.com>
2020-09-24 15:16:50 +0900

    emailjs사용
    Former-commit-id: 825af4b981b1f1c01d7b5bcd7aa3302ef1216dd9

--- Service/src/store/index.js
+++ Service/src/store/index.js
@@ -1,34 +1,107 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import emailjs from "emailjs-com";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
+  state: {},
+  mutations: {},
   actions: {
-    login: (context, loginData)=>{
-      console.log("store login "+loginData.uid+" "+loginData.upw);
-    },
-    join: (context, joinData)=>{
-      console.log("store join : "+joinData.uid);
+    login: (context, loginData) => {
+      console.log("store login " + loginData.uid + " " + loginData.upw);
       axios
-          .post("http://localhost:8080/Userinfo/join", joinData)
-          .then(({ data }) => {
-            console.log(data);
-            console.log(data.Userinfo);
-            console.log(data.Userinfo.uid);
+        .post("http://localhost:8080/Userinfo/login", loginData)
+        .then(({ data }) => {
+          if (data != null) {
+            alert("로그인 성공");
+            localStorage.setItem("NowID", loginData.uid);
+          }
+        })
+        .catch(() => {
+          alert("로그인 시 에러가 발생했습니다.");
+        });
+    },
+    join: (context, joinData) => {
+      console.log("store join : " + joinData.uid);
+      axios
+        .post("http://localhost:8080/Userinfo/join", joinData)
+        .then(({ data }) => {
+          console.log(data);
+          console.log(data.Userinfo);
+          console.log(data.Userinfo.uid);
+        })
+        .catch(() => {
+          alert("로그인 시 에러가 발생했습니다.");
+        });
+    },
+    findpw: (context, findpwData) => {
+      console.log("store findpw [email] : " + findpwData.target_email);
+      // axios를 통해서 백엔드와 통신. [사용자가 입력한 이메일을 인자로 넘기고, 그걸 받은 백엔드에서 DB와 비교. 가입된 이메일인지 체크.]
+      // 가입된 이메일이라면 true, 아니라면 false 값 반환. 숫자 0 또는 1로 반환해도 가능.
 
-          })
-          .catch(() => {
-            alert("로그인 시 에러가 발생했습니다.");
-          });
-    }
+      // 반환된 값을 기준으로 가입된 이메일이라면 비밀번호찾는 이메일을 입력된 이메일에 보내고,
+      // 가입된 이메일이 아니라면 가입한 이메일을 입력하라는 경고창 팝업.
+
+      emailjs
+        .send(
+          "service_AI",
+          "template_r1n19h8",
+          findpwData,
+          "user_5j1guKc2v1FQ1OJzyA1rf"
+        )
+        .then(
+          function(response) {
+            console.log("SUCCESS!", response.status, response.text);
+          },
+          function(err) {
+            console.log("FAILED...", err);
+          }
+        );
+
+      axios
+        .post("http://localhost:8080/mmlmlml", {
+          email: findpwData.target_email,
+        })
+        .then(({ data }) => {
+          this.item = data;
+          let msg = "가입된 이메일이 아닙니다.";
+          if (this.item.email != null) {
+            msg = "입력하신 이메일로 접속해 Code를 확인해주세요.";
+          }
+          // alert(this.item.email);
+          // alert(this.item);
+          alert(msg);
+          let ranNum = "" + Math.floor(Math.random() * 1000);
+          if (this.item.email != null) {
+            localStorage.setItem("email", this.item.email);
+            this.findpwData.message_html += ranNum;
+            localStorage.setItem("code", ranNum);
+            emailjs
+              .send("service_AI", "template_r1n19h8", this.findpwData)
+              .then(
+                function(response) {
+                  console.log("SUCCESS!", response.status, response.text);
+                },
+                function(err) {
+                  console.log("FAILED...", err);
+                }
+              );
+            alert("메일전송");
+            this.$router.push("/");
+
+            var myWindow = window.open("", "_self");
+            myWindow.document.write("");
+            setTimeout(function() {
+              myWindow.close();
+            }, 1000);
+          }
+        })
+        .catch(() => {
+          alert("진행 중 에러가 발생했습니다.");
+          window.location.href = "/";
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- Service/src/views/Users.vue
+++ Service/src/views/Users.vue
@@ -1,41 +1,32 @@
 <template>
   <v-container>
-    <v-row
-        align="center"
-        justify="center"
-    >
-      <v-col
-          cols="12"
-          sm="8"
-          md="4"
-      >
-      <userLoginAJoin></userLoginAJoin>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <userLoginAJoin></userLoginAJoin>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import userLoginAJoin from '@/components/Users/newlog'
+import userLoginAJoin from "@/components/Users/newlog";
 import axios from "axios";
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       useremail: "",
       userpwd: "",
-      U_id: "",
+      U_nickname: "",
       U_email: "",
       U_password1: "",
       U_password2: "",
-      // avatarImage: require("../../assets/img/person.png"),
-      usercomment: "Hi, Nice to meet you.",
-      // usertoken: '',
-      item: {},
+      item1: {},
+      item2: {},
     };
   },
   components: {
-    userLoginAJoin
+    userLoginAJoin,
   },
   computed: {
     ...mapGetters(["token"]),
@@ -43,68 +34,51 @@ export default {
   methods: {
     loginHandler() {
       axios
-          .post("http://localhost:8080/user/login", {
-            email: this.useremail,
-            password: this.userpwd,
-          })
-          .then(({ data }) => {
-            this.item = data;
-            // alert(this.item);
-            // alert(this.item.email);
-            let msg = "로그인 정보가 정확하지 않습니다.";
-            if (this.item.email != null) {
-              msg = "로그인이 완료되었습니다.";
-            }
-            alert(msg);
-            if (this.item.email != null) {
-              localStorage.setItem("nowUid", this.item.uid);
-              localStorage.setItem("nowEmail", this.useremail);
-              // 실제 유저프로필 이미지. avatarImage.
-              localStorage.setItem("avatarImage", this.item.avatarImage);
-              // 유저프로필 이미지의 저장경로. avatarImageSrc.
-              localStorage.setItem("avatarImageSrc", this.item.avatarImageSrc);
-              localStorage.setItem("usercomment", this.item.usercomment);
-              // alert('토큰 정보'+this.item.token);
-              // this.usertoken = this.item.token;
-              localStorage.setItem("nowToken", this.item.token);
-              alert("토큰 생성이 완료됐습니다.");
-              window.location.href = "/main";
-            }
-          })
-          .catch(() => {
-            alert("로그인 시 에러가 발생했습니다.");
+        .post("http://localhost:8080/user/login", {
+          email: this.useremail,
+          password: this.userpwd,
+        })
+        .then(({ data }) => {
+          this.item1 = data;
+          let msg = "로그인 정보가 정확하지 않습니다.";
+          if (this.item1.email != null) {
+            msg = "로그인이 완료되었습니다.";
+          }
+          alert(msg);
+          if (this.item1.email != null) {
+            localStorage.setItem("nowNickName", this.item1.uid);
+            localStorage.setItem("nowEmail", this.useremail);
             window.location.href = "/";
-          });
+          }
+        })
+        .catch(() => {
+          alert("로그인 시 에러가 발생했습니다.");
+          window.location.href = "/";
+        });
     },
     registerHandler() {
       axios
-          .post("http://localhost:8080/user/signUp", {
-            uid: this.U_id,
-            email: this.U_email,
-            password: this.U_password1,
-            interest1: "",
-            interest2: "",
-            interest3: "",
-            avatarImage: this.avatarImage,
-            imgtype: "image/png",
-            usercomment: "Hi, Nice to meet you.",
-          })
-          .then(({ data }) => {
-            this.isok = data;
-            // alert(this.isok);
-            let msg = "회원가입실패.";
-            if (this.isok != null) {
-              msg = "회원가입성공.";
-            }
-            alert(msg);
-            if (this.isok != null) {
-              window.location.href = "/";
-            }
-          })
-          .catch(() => {
-            alert("회원가입 시 에러가 발생했습니다.");
-            window.location.href = "/";
-          });
+        .post("http://localhost:8080/user/signUp", {
+          nickname: this.U_nickname,
+          email: this.U_email,
+          password1: this.U_password1,
+          password2: this.U_password2,
+        })
+        .then(({ data }) => {
+          this.item2 = data;
+          let msg = "회원가입실패.";
+          if (this.item2 != null) {
+            msg = "회원가입성공.";
+          }
+          alert(msg);
+          if (this.item2 != null) {
+            window.location.href = "/users";
+          }
+        })
+        .catch(() => {
+          alert("회원가입 시 에러가 발생했습니다.");
+          window.location.href = "/";
+        });
     },
   },
 };
